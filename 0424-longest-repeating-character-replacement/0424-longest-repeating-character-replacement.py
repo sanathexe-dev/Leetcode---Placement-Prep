c1096803ad117b6f8768802a0e50f36f163acfdd
sanathexe-dev <sanath.exe@gmail.com>
2026-07-22 19:24:05 +0530

    Time: 195 ms (13.08%), Space: 19.7 MB (50.03%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -12,17 +12,6 @@ class Solution:
         return res
 
 
-        
-        l=0
-        res=0
-        count=[0]*26
-        for r in range(len(s)):
-            count[ord(s[r])-65]+=1
-            while (r-l+1)-max(count)>k:
-                count[ord(s[l])-65]-=1
-                l+=1
-            res=max(res,r-l+1)
-        return res
             
 
         
