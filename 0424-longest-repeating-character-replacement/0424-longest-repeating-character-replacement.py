27b677d52e9f737e7dc3a4aef222438149a880dc
sanathexe-dev <sanath.exe@gmail.com>
2026-07-22 19:20:14 +0530

    Time: 195 ms (13.08%), Space: 19.7 MB (50.03%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,8 +1,18 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        count=[0]*26
+        l=0
+        res=0
+        for r in range(len(s)):
+            count[ord(s[r])-65]+=1
+            while (r-l+1) - max(count)>k:
+                count[ord(s[l])-65]-=1
+                l+=1
+            res=max(res,r-l+1)
+        return res
 
 
-
+        
         l=0
         res=0
         count=[0]*26
@@ -13,3 +23,9 @@ class Solution:
                 l+=1
             res=max(res,r-l+1)
         return res
+            
+
+        
+       
+
+
