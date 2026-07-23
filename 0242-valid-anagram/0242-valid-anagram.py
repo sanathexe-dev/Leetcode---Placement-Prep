55325e9037ad6e2552de5ab65be4c101106e58fc
sanathexe-dev <sanath.exe@gmail.com>
2026-07-23 09:20:35 +0530

    Time: 11 ms (77.02%), Space: 19.4 MB (75.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,12 +4,11 @@ class Solution:
             return False
         count1=[0]*26
         count2=[0]*26
-
-        for i in range(len(s)):
-            count1[ord(s[i])-97]+=1
-            count2[ord(t[i])-97]+=1
+        for i in s:
+            count1[ord(i)-97]+=1
+        for i in t:
+            count2[ord(i)-97]+=1
         if count1==count2:
             return True
-        else:
-            return False
+        return False
         
