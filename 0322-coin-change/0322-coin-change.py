51d28f0d04edd9e8136f41141a559d3aa6b03679
sanathexe-dev <sanath.exe@gmail.com>
2026-07-24 23:36:11 +0530

    Time: 488 ms (67.66%), Space: 19.6 MB (78.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,17 +2,16 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         coins.sort()
         dp=[0]*(amount+1)
-
         for i in range(1,amount+1):
             minn=float("inf")
-
             for coin in coins:
                 diff=i-coin
                 if diff<0:
                     break
                 minn=min(minn,dp[diff]+1)
             dp[i]=minn
-        if dp[-1]==float("inf"):
-            return -1
+        if dp[amount]!=float("inf"):
+            return dp[amount]
         else:
-            return dp[-1]
+            return -1
+
