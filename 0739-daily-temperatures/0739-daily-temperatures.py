af2fe15fa50da1e76073c8c83e10f962224fcb17
sanathexe-dev <sanath.exe@gmail.com>
2026-07-24 19:02:16 +0530

    Time: 84 ms (85.99%), Space: 28.8 MB (37.42%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,17 +1,15 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
 
-
         temp=temperatures
-        stk=[]
         res=[0]*len(temp)
+        stk=[]
         for i in range(len(temp)):
             while stk and temp[stk[-1]]<temp[i]:
                 idx=stk.pop()
                 res[idx]=i-idx
             stk.append(i)
         return res
-
 
                 
         
