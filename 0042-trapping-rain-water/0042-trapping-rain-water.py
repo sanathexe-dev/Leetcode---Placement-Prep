6dde0b7fc98c1d295a3e55313e3b4736d24d602e
sanathexe-dev <sanath.exe@gmail.com>
2026-07-25 10:36:58 +0530

    Time: 6 ms (77.15%), Space: 21 MB (47.94%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,15 +1,12 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-
-
-
         l=0
         r=len(height)-1
         res=0
-        MR=height[r]
         ML=height[l]
+        MR=height[r]
         while l<r:
-            if ML<MR:
+            if MR>ML:
                 l+=1
                 ML=max(ML,height[l])
                 res+=ML-height[l]
@@ -18,5 +15,6 @@ class Solution:
                 MR=max(MR,height[r])
                 res+=MR-height[r]
         return res
+
 
         
