832c57429b374501f931eef06a46f7bed0f88cf5
sanathexe-dev <sanath.exe@gmail.com>
2026-07-28 19:07:14 +0530

    Time: 7 ms (91.57%), Space: 19.4 MB (45.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,12 +2,12 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        count1={}
-        count2={}
-        for i in s:
-            count1[i]=count1.get(i,0)+1
+        count1=[0]*26
+        count2=[0]*26
         for i in t:
-            count2[i]=count2.get(i,0)+1
+            count1[ord(i)-97]+=1
+        for i in s:
+            count2[ord(i)-97]+=1
         return count1==count2
 
         
