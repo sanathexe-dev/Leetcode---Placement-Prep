30b4cd9db883acb3ecdd3ca92053199db78afd99
sanathexe-dev <sanath.exe@gmail.com>
2026-07-28 20:18:54 +0530

    Time: 0 ms (100%), Space: 19.4 MB (10.63%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -11,8 +11,9 @@ class Solution:
             for i in range(2,len(nums)):
                 dp[i]=max(dp[i-1],dp[i-2]+nums[i])
             return dp[-1]
-        
         return max(getmax(nums[1:]),getmax(nums[:-1]))
+
+        
 
         
         
