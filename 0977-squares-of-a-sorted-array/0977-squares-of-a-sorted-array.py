723dae6b95d8395074115a34eabe51318b071157
sanathexe-dev <sanath.exe@gmail.com>
2026-08-28 16:21:24 +0530

    Time: 10 ms (57.97%), Space: 21.1 MB (28.21%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -3,7 +3,6 @@ class Solution:
         l=0
         r=len(nums)-1
         res=[0]*len(nums)
-
         for i in range(len(nums)-1,-1,-1):
             if abs(nums[l])>abs(nums[r]):
                 res[i]=nums[l]**2
